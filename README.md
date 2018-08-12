227edca2585e18ab95ea944fb9f901d05176db27
Yu-Han <yuhanluo1994@gmail.com>
2018-08-12 21:46:16 +0800

    add README

---
+++ README.md
@@ -0,0 +1,17 @@
+# psychopy_tobii_infant
+
+Create an infant-friendly eyetracking study with PsychoPy and psychopy_tobii_controller.
+
+## Author
+
+Yu-Han Luo
+
+## Dependency
+
+[PsychoPy](psychopy_tobii_controller)  
+[tobii_research](https://pypi.python.org/pypi/tobii-research)  
+[psychopy_tobii_controller](https://github.com/hsogo/psychopy_tobii_controller)
+
+## License
+
+GPL v3.0 or later
--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='psychopy_tobii_infant',
       version='0.1',
       description='Infant-friendly eyetracking',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='http://myrepo',  # FIXME: 
       author='Yu-Han Luo',
       author_email='yuhanluo1994@gmail.com',
       license='GPL v3.0 or later',
       packages=['psychopy_tobii_infant'],
+      dependency_links=['https://github.com/hsogo/psychopy_tobii_controller'],
       zip_safe=False)
