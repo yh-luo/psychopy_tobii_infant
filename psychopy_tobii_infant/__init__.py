e2db2e61dfd431fb05ebdf1fb1947c4c31ddb3bd
Yu-Han Luo <yuhanluo1994@gmail.com>
2018-08-13 18:53:54 +0800

    FF: fix the methods and add some documentation
    This packages is built on Python 3, but should be able to run on Python 2.

--- psychopy_tobii_infant/__init__.py
+++ psychopy_tobii_infant/__init__.py
@@ -5,16 +5,61 @@ import psychopy_tobii_controller
 import numpy as np
 
 from psychopy import visual, event, core
+from psychopy.constants import PY3
+
+# check python version
+if PY3:
+    import types
+
+
+def set_custom_calibration(self, func):
+    """Set custom calibration function.
+    
+    Because psychopy_tobii_controller was written in Python 2, the method
+    will be reassigned for Python 3 compatabillity. Users may not use this
+    function.
+
+    Args:
+        func: custom calibration function.
+
+    Returns:
+        None
+    """
+
+    self.update_calibration = types.MethodType(func, self)
 
 
 def infant_show_status(self,
-                       attr,
+                       att_stim,
                        enable_mouse=False,
                        pos=[0, 0],
                        size=[640, 480],
                        units='pix'):
+    """Infant-friendly procedure to adjust the participant's position.
+
+    This is an implementation of show_status() in psychopy_tobii_controller. It
+    plays an interesting video to attract the participant's attention and
+    map the relative position of eyes to the track box. The experimenter can
+    thus inspect and adjust the position of the participant.
+
+    Args:
+        att_stim: the video to be played.
+        enable_mouse: use mouse clicks to leave the procedure?
+        pos: the position to draw the video.
+        size: the size of the video.
+        units: the units of parameters (see PsychoPy manual for more info).
+
+    Returns:
+        None
+    """
     attention_grabber = visual.MovieStim3(
-        self.win, attr, pos=pos, size=size, units=units, name=attr, autoLog=False)
+        self.win,
+        att_stim,
+        pos=pos,
+        size=size,
+        units=units,
+        name=att_stim,
+        autoLog=False)
 
     bgrect = visual.Rect(
         self.win,
@@ -119,23 +164,46 @@ def infant_show_status(self,
             attention_grabber.draw()
             self.win.flip()
         if b_show_status:
-            attention_grabber.loadMovie(attr)
+            attention_grabber.loadMovie(att_stim)
 
     attention_grabber.stop()
     self.eyetracker.unsubscribe_from(tobii_research.EYETRACKER_GAZE_DATA)
 
-# infant calibration
-def infant_calibration(self, stims):
-    """Customized calibration for infants."""
-    # mapping key name to target index
-    numkey_dict = {
-        'num_0': -1,
-        'num_1': 0,
-        'num_2': 1,
-        'num_3': 2,
-        'num_4': 3,
-        'num_5': 4
-    }
+
+def infant_calibration(self,
+                       stims,
+                       numkey_dict=None,
+                       collect_key='space',
+                       exit_key='return'):
+    """Customized calibration for infants.
+
+    An implementation of run_calibration() in psychopy_tobii_controller,
+    achieved by set_custom_calibration().
+
+    Args:
+        stims: calibration stimuli (should be the same number of calibration
+            point).
+        numkey_dict: keymap for manual calibration. Default to five-point
+            calibration, using 1~5 to present calibration stimulus and 0 to
+            hide the target.
+        collect_key: key to start collecting samples.
+        exit_key: key to finish and leave the current calibration procedure.
+
+    Returns:
+        None
+    """
+
+    if numkey_dict is None:
+        # the keymap for target index
+        numkey_dict = {
+            'num_0': -1,
+            'num_1': 0,
+            'num_2': 1,
+            'num_3': 2,
+            'num_4': 3,
+            'num_5': 4
+        }
+
     current_point_index = -1
     # prepare calibration stimuli
     cali_targets = [visual.ImageStim(self.win, image=v) for v in stims]
@@ -153,14 +221,14 @@ def infant_calibration(self, stims):
         for key in keys:
             if key in numkey_dict:
                 current_point_index = numkey_dict[key]
-            elif key == 'space':
+            elif key == collect_key:
                 # collect samples when space is pressed
                 if current_point_index in self.retry_points:
                     self.collect_calibration_data(
                         self.calibration_points[current_point_index])
-                    # deactivate the target
+                    # deactivate the target after colleting samples
                     current_point_index = -1
-            elif key == 'return':
+            elif key == exit_key:
                 # exit calibration when return is presssed
                 in_calibration = False
                 break
@@ -175,11 +243,18 @@ def infant_calibration(self, stims):
             cali_targets[current_point_index].draw()
         self.win.flip()
 
-def get_current_gaze_validity(self):
-    """
-    Get current gaze validity provided by Tobii eye tracker as a tuple of
-    (time, left_validity, right_validity)
 
+def get_current_gaze_validity(self):
+    """Get gaze validity.
+
+    Get current gaze validity provided by Tobii eye tracker as a tuple of
+    (left_validity, right_validity). Users may not call this function.
+
+    Args:
+        None
+    Returns:
+        lv: validity of left-eye gaze point
+        rv: validity of right-eye gaze point
     """
 
     if len(self.gaze_data) == 0:
@@ -191,8 +266,21 @@ def get_current_gaze_validity(self):
 
 
 # Collect looking time
-def collect_lt(self, max_time, min_away, hz=60, blink_dur=1):
-    """Collect looking time data and determine the end of looking"""
+def collect_lt(self, max_time, min_away, blink_dur=1):
+    """Collect looking time data in runtime
+
+    Collect and calculate looking time in runtime. Also end the trial
+    automatically when the participant look away.
+
+    Args:
+        max_time: maximum looking time in seconds.
+        min_away: minimum duration to stop in seconds.
+        blink_dur: the tolerable duration of missing data in seconds.
+
+    Returns:
+        the looking time in the trial.
+    """
+
     trial_timer = core.CountdownTimer(max_time)
     absence_timer = core.CountdownTimer(min_away)
     away = []
@@ -222,3 +310,24 @@ def collect_lt(self, max_time, min_away, hz=60, blink_dur=1):
     else:
         lt = max_time - np.sum(away)
         return (round(lt, 3))
+
+
+if PY3:
+    psychopy_tobii_controller.tobii_controller.set_custom_calibration = set_custom_calibration
+    psychopy_tobii_controller.tobii_controller.get_current_gaze_validity = types.MethodType(
+        get_current_gaze_validity, psychopy_tobii_controller.tobii_controller)
+    psychopy_tobii_controller.tobii_controller.collect_lt = types.MethodType(
+        collect_lt, psychopy_tobii_controller.tobii_controller)
+    psychopy_tobii_controller.tobii_controller.infant_show_status = types.MethodType(
+        infant_show_status, psychopy_tobii_controller.tobii_controller)
+
+else:
+    psychopy_tobii_controller.tobii_controller.get_current_gaze_validity = types.MethodType(
+        get_current_gaze_validity, psychopy_tobii_controller.tobii_controller,
+        psychopy_tobii_controller.tobii_controller)
+    psychopy_tobii_controller.tobii_controller.collect_lt = types.MethodType(
+        collect_lt, psychopy_tobii_controller.tobii_controller,
+        psychopy_tobii_controller.tobii_controller)
+    psychopy_tobii_controller.tobii_controller.infant_show_status = types.MethodType(
+        infant_show_status, psychopy_tobii_controller.tobii_controller,
+        psychopy_tobii_controller.tobii_controller)
