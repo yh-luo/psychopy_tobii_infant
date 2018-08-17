3e0698b3662ccb817e9fb14eed3c4ecd316b027c
Yu-Han Luo <yuhanluo1994@gmail.com>
2018-08-17 17:30:12 +0800

    DOC: update README.md and examples.

--- README.md
+++ README.md
@@ -1,20 +1,69 @@
 # psychopy_tobii_infant
 
 Create infant-friendly eyetracking experiments with PsychoPy and psychopy_tobii_controller.
-This package was mostly based on psychopy_toii_controller, with some modification and extra methods to make life easier for developmentalists and anyone who works with children.
 
-**This project is under development.**
+## Who it's for
 
-### Author
+This package was mostly based on psychopy_toii_controller, with some some modifications to make life easier for anyone who works with infants and children.
+
+### Cautions
+
+- This project is unofficial.
+- This project is under development.
+- Test the scripts thoroughly before jumping into data collection!
+
+## Installation
+
+### Use git
+
+1. Clone this folder
+    ```bash
+    git clone https://github.com/yh-luo/psychopy_tobii_infant.git
+    ```
+2. Navigate to the folder
+3. install the package with `pip`
+    ```python
+    pip install .
+    ```
+
+### Download the ZIP file
+
+1. Download this folder (the green button)
+2. Unzip the file and navigate to the folder
+3. Install the package with `pip`
+    ```python
+    pip install .
+    ```
+
+## Author
 
 Yu-Han Luo
+
+## Requirements
+
+### Python 3.5.x
+
+Currently tested on Python 3.5.6
 
 ### Dependency
 
 - [PsychoPy](psychopy_tobii_controller)  
 - [tobii_research](https://pypi.python.org/pypi/tobii-research)  
-- [psychopy_tobii_controller](https://github.com/hsogo/psychopy_tobii_controller)
+- [psychopy_tobii_controller](https://github.com/hsogo/psychopy_tobii_controller) by Hiroyuki Sogo
 
-### License
+## Examples
+
+### demo1_calibration.py
+
+1. Show the relative position of the subject to the eyetracker.
+2. Run five-points calibration
+
+### demo2_collect_looking_time.py
+
+1. Show the relative position of the subject to the eyetracker.
+2. Run five-points calibration
+3. Collect looking time data based on the eyetracker
+
+## License
 
 GPL v3.0 or later
---
+++ demo/demo1_calibration.py
@@ -0,0 +1,76 @@
+# Calibration
+import numpy as np
+import os
+
+from psychopy import visual, event, core, prefs
+prefs.general['audioLib'] = ['sounddevice']
+
+from psychopy_tobii_infant import infant_tobii_controller
+
+###############################################################################
+# Constants
+###############################################################################
+DIR = os.path.dirname(__file__)
+# users should know the display well.
+DISPSIZE = (34, 27)
+# define calibration points
+CALINORMP = [(-0.4, 0.4), (-0.4, -0.4), (0.0, 0.0), (0.4, 0.4), (0.4, -0.4)]
+CALIPOINTS = [(x * DISPSIZE[0], y * DISPSIZE[1]) for x, y in CALINORMP]
+# correct path for calibration stimuli
+CALISTIMS = [
+    'infant/{}'.format(x) for x in os.listdir(os.path.join(DIR, 'infant'))
+    if '.png' in x
+]
+
+###############################################################################
+# Demo
+###############################################################################
+# create a Window to control the monitor
+win = visual.Window(
+    size=[1280, 1024],
+    monitor='tobii',
+    units='cm',
+    screen=1,
+    fullscr=True,
+    allowGUI=False)
+
+gaze = visual.Circle(
+    win,
+    size=0.02,
+    lineColor=None,
+    fillColor='white',
+    autoLog=False)
+
+# initialize tobii_controller to communicate with the eyetracker
+controller = infant_tobii_controller(win)
+# Open a data file to save gaze data
+controller.open_datafile('test_infant_calibration.tsv')
+
+# show the status of eye tracker
+controller.show_status("infant/elmo's ducks.mp4")
+
+ret = controller.run_calibration(CALIPOINTS, CALISTIMS, start_key=None)
+
+if ret == 'abort':
+    core.quit()
+
+controller.subscribe()
+running = True
+while running:
+    currentGazePosition = controller.get_current_gaze_position()
+    if np.nan not in currentGazePosition:
+        gaze.setPos(currentGazePosition[0:2])
+
+    keys = event.getKeys()
+    for key in keys:
+        if key == 'space':
+            running = False
+
+    gaze.draw()
+    win.flip()
+
+controller.unsubscribe()
+controller.close_datafile()
+
+win.close()
+core.quit()
