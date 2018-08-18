af4643efd8213d6548841d53a846e386feb64a01
Yu-Han Luo <yuhanluo1994@gmail.com>
2018-08-18 10:13:17 +0800

    FF: add new demo

--- demo/demo3.py
+++ demo/demo3.py
@@ -1,5 +1,5 @@
-# Calibration
-import numpy as np
+# collect looking time while playing a video
+import random
 import os
 
 from psychopy import visual, event, core, prefs
@@ -10,6 +10,8 @@ from psychopy_tobii_infant import infant_tobii_controller
 ###############################################################################
 # Constants
 ###############################################################################
+random.seed()
+
 DIR = os.path.dirname(__file__)
 # users should know the display well.
 DISPSIZE = (34, 27)
@@ -26,6 +28,8 @@ CALISTIMS = [
 # Demo
 ###############################################################################
 # create a Window to control the monitor
+# It is assumed that the profile of the monitor is know. Thus, stimuli in
+# show_status are in 'height' units.
 win = visual.Window(
     size=[1280, 1024],
     monitor='tobii',
@@ -34,19 +38,12 @@ win = visual.Window(
     fullscr=True,
     allowGUI=False)
 
-gaze = visual.Circle(
-    win,
-    size=0.02,
-    lineColor=None,
-    fillColor='white',
-    autoLog=False)
-
 # initialize tobii_controller to communicate with the eyetracker
 controller = infant_tobii_controller(win)
-# Open a data file to save gaze data
+# Open a file to save the eyetracking data
 controller.open_datafile('test_infant_calibration.tsv')
 
-# show the status of eye tracker
+# show the relative position of the subject to the eyetracker
 controller.show_status("infant/elmo's ducks.mp4")
 
 ret = controller.run_calibration(CALIPOINTS, CALISTIMS, start_key=None)
@@ -54,22 +51,21 @@ ret = controller.run_calibration(CALIPOINTS, CALISTIMS, start_key=None)
 if ret == 'abort':
     core.quit()
 
+# prepare the video
+movie = visual.MovieStim3(
+    win, 'infant/v1_h_r.mp4', size=[600, 600], units='pix', loop=True)
+movie.setAutoDraw(True)
+
 controller.subscribe()
-running = True
-while running:
-    currentGazePosition = controller.get_current_gaze_position()
-    if np.nan not in currentGazePosition:
-        gaze.setPos(currentGazePosition[0:2])
+# wait a bit for the eyetracker to turn on
+core.wait(0.5)
 
-    keys = event.getKeys()
-    for key in keys:
-        if key == 'space':
-            running = False
+# start
+controller.collect_lt_mov(movie, 10, 2)
 
-    gaze.draw()
-    win.flip()
-
+# stop recording
 controller.unsubscribe()
+# close the file
 controller.close_datafile()
 
 win.close()
